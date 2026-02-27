80ea1f6b1b6c79c7218beb1658b96ecd3d6ae66d
fwhat74 <fwhats74@gmail.com>
2026-02-27 22:06:57 +0530

    Update number.js

--- api/number.js
+++ api/number.js
@@ -25,14 +25,13 @@ export default function handler(req, res) {
   if (!number) {
     return res.status(400).json({
       success: false,
-      error: 'Use: /api/number?number=XXXXXXXXXX'
+      error: 'Use: /api/number?number=919606001060'
     });
   }
 
   try {
     const data = loadCsv();
 
-    // ⚠️ Yaha column name EXACT match hona chahiye CSV header se
     const result = data.find(row => row.Number === number);
 
     if (!result) {
@@ -48,6 +47,7 @@ export default function handler(req, res) {
     });
 
   } catch (err) {
+    console.error(err);
     return res.status(500).json({
       success: false,
       error: 'Server error'
